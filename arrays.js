8c50eed611f0a05ead0a1994652b07e1bd09260e
Afia Kyalo <afiakyalo@gmail.com>
2019-06-20 00:06:39 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,9 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
-function addElementToBeginningOfArray() {
+function addElementToBeginningOfArray([], ) {
   
 }
 
-function 
+function destructivelyAddElementToBeginningOfArray() {
+  
+}
